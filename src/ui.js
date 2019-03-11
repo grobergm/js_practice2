09ab071443eaf42caea82c464ab0be06f0b4cfc0
Leo Mangutov and Matt Groberg <leo@>
2019-03-11 12:01:43 -0700

    add rest of UI display values

--- src/ui.js
+++ src/ui.js
@@ -8,6 +8,9 @@ $(document).ready(function() {
     var inputContent = $("#content").val();
     var newEntry = new Entry(inputTitle, inputContent);
     var newWords = newEntry.words();
-    $("#results").append(newEntry.content + newWords);
+    var newVowels = newEntry.charType().vowels;
+    var newConsonents = newEntry.charType().consonants;
+
+    $("#results").append(`<h1>${newEntry.title}</h1><p>${newEntry.content}</p><p>There are ${newWords} words</p><p>${newVowels}</p><p>${newConsonents}</p>`);
   });
 });
