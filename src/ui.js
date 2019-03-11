59f8b73e9b51b02f246b231283c6c71eac583d54
Leo Mangutov and Matt Groberg <leo@>
2019-03-11 13:00:53 -0700

    add some content to explain outputed values

--- src/ui.js
+++ src/ui.js
@@ -11,6 +11,6 @@ $(document).ready(function() {
     var newVowels = newEntry.charType().vowels;
     var newConsonents = newEntry.charType().consonants;
 
-    $("#results").append(`<h1>${newEntry.title}</h1><p>${newEntry.content}</p><p>There are ${newWords} words</p><p>${newVowels}</p><p>${newConsonents}</p>`);
+    $("#results").append(`<h1>${newEntry.title}</h1><p>${newEntry.content}</p><p>There are ${newWords} words</p><p>Here are the vowels: ${newVowels}</p><p>Here are the consonents: ${newConsonents}</p>`);
   });
 });
